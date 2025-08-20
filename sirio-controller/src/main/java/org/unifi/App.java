31d1b893804d70483a31b0828669d55745638b5c
Carlo Lucchesi <carlo.lucchesi@edu.unifi.it>
2025-08-20 13:14:40 +0200

    Initialization of the java project for the controller.
    Creation of the Maven project for developing the controller using Sirio.

---
+++ sirio-controller/src/main/java/org/unifi/App.java
@@ -0,0 +1,10 @@
+package org.unifi;
+
+import org.oristool.petrinet.PetriNet;
+
+public class App {
+    public static void main(String[] args) {
+        PetriNet pn = new PetriNet();
+        System.out.println("Hello World!");
+    }
+}
